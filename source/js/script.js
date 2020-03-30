babff5374fec1ba59ae0d2d5e03e1da5b33f9133
Tikondra <tikondra47@gmail.com>
2020-03-30 11:05:21 +0400

    manufacture

--- source/js/script.js
+++ source/js/script.js
@@ -8,7 +8,7 @@
 
   menuBtn.addEventListener('click', function () {
     menu.classList.toggle('navigation--show');
-    menuBtn.classList.toggle('header__open-menu--active');
+    menuBtn.classList.toggle('header__nav-btn--active');
     btnSvg.classList.toggle('active');
   });
 })();
